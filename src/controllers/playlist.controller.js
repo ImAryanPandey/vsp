5946253f28df38b0cde7936c2478eab64c1931fc
Aryan Pandey <aryan513pandey@gmail.com>
2025-09-27 06:32:43 +0000

    Playlist Controllers and Routes

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,11 +1,196 @@
 import mongoose from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const { name, description } = req.body;
 
-    if (!name || !description) {
-        throw new ApiError();
+    if (!name || name.trim() === "") {
+        throw new ApiError(400, "Name cannot be empty");
     }
+
+    if(!description || deescription.trim() === ""){
+        throw new ApiError(400, "Description cannot be empty");
+    }
+
+    const playlist = await Playlist.create({
+        name,
+        description,
+        owner: req.user._id
+    });
+
+    if (!playlist) {
+        throw new ApiError(500, "Failed to create playlist");
+    }
+
+    return res.status(201).json(
+        new ApiResponse(201, playlist, "Playlist created successfully")
+    )
 });
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const { userId } = req.user._id;
+
+    if (!mongoose.isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user ID");
+    }
+
+    const playlists = await Playlist.find({ owner: userId});
+
+    return res.status(200).json(
+        new ApiResponse(200, playlists, "User playlists retrieved successfully")
+    )
+
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const { playlistId } = req.params;
+
+    if (!mongoose.isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID");
+    }
+    const playlist = await Playlist.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(playlistId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "videos",
+                foreignField: "_id",
+                as: "videos"
+            }
+        }
+    ]);
+
+    if (!playlist || playlist.length === 0) {
+        throw new ApiError(404, "Playlist not found");
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200, playlist[0], "Playlist retrieved successfully")
+    )
+
+});
+
+const addVideoToPlayList = asyncHandler(async (req, res) => {
+    const { playlistId, videoId } = req.params;
+
+    if (!mongoose.isValidObjectId(playlistId) || !mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid playlist or video ID");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+    const video = await Video.findById(videoId);
+
+    if(!playlist) throw new ApiError(404, "Playlist not found");
+    if(!video) throw new ApiError(404, "Video not found");
+
+    if(playlist.owner.toString() !== req.user._id.toString()){
+        throw new ApiError(403, "Only owner have permission to modify this playlist");
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $addToSet: { videos: videoId }
+        },
+        { new: true }
+    );
+
+    return res.status(200).json(
+        new ApiResponse(200, updatedPlaylist, "Video added to playlist successfully")
+    )
+
+});
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+    const { playlistId, videoId } = req.params;
+
+    if (!mongoose.isValidObjectId(playlistId) || !mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid playlist or video ID");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist) throw new ApiError(404, "Playlist not found");
+
+    if(playlist.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403, "Only owner have permission to modify this playlist");
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull: { videos: videoId }
+        },
+        { new: true }
+    );
+
+    return res.status(200).json(
+        new ApiResponse(200, updatedPlaylist, "Video removed from playlist successfully")
+    );
+});
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+    const { playlistId } = req.params;
+
+    if (!mongoose.isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid Playlist ID");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) throw new ApiError(404, "Playlist not found");
+
+    if (playlist.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "Only the owner can delete this playlist");
+    }
+
+    await Playlist.findByIdAndDelete(playlistId);
+
+    return res.status(200).json(
+        new ApiResponse(200, {}, "Playlist deleted successfully")
+    );
+});
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+    const { playlistId } = req.params;
+    const { name, description } = req.body;
+
+    if (!name || !description) {
+        throw new ApiError(400, "Name and description are required");
+    }
+
+    if (!mongoose.isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid Playlist ID");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) throw new ApiError(404, "Playlist not found");
+
+    if (playlist.owner.toString() !== req.user?._id.toString()) {
+        throw new ApiError(403, "Only the owner can update this playlist");
+    }
+
+    const updatedPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set: {
+                name,
+                description
+            }
+        },
+        { new: true }
+    );
+
+    return res.status(200).json(
+        new ApiResponse(200, updatedPlaylist, "Playlist updated successfully")
+    );
+});
+
+export { createPlaylist, getUserPlaylists, getPlaylistById, addVideoToPlayList, removeVideoFromPlaylist, deletePlaylist, updatePlaylist };
